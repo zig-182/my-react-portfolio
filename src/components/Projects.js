55b2ba2e94aae43777c21573740e8273ea7111d1
zig-182 <zigsaver@gmail.com>
2024-08-15 17:02:22 +0100

    Fourth commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,6 +19,7 @@ export default function Projects() {
           {projects.map((project) => (
             <a
               href={project.link}
+              target="_blank"
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
               <div className="flex relative">
